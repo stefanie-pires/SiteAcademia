bfb6416bbead9ca9738b44e9d70c09ca1837cf3a
stefanie-pires <stefanie.gs.sg@gmail.com>
2024-10-01 22:10:49 -0300

    feat: atualiza front

--- src/Components/Informacoes/index.js
+++ src/Components/Informacoes/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import styles from './Informações.module.css';
+import styles from './Informacoes.module.css';
 
-function Informações() {
+function Informacoes() {
   return (
     <section className={styles.informacoes}>
-      <h3>Horário de Atendimento</h3>
-      <p>Segunda a Sexta-feira: 05:00-22:00</p>
-      {/* ... outras informações */}
+      <h3>Informações:</h3>
+      <div className={styles.informacoesContainer}> {/* Mudança aqui para 'informacoesContainer' */}
+        <p className={styles.noBreak}> {/* Mudança aqui para 'noBreak' */}
+          Estamos localizados no coração de Curitiba, no endereço: 
+          <strong> Rua das Araucarias, n 14,</strong> um local acessível e 
+          conveniente para todas as nossas alunas.
+        </p>
+        <p>
+          Visite-nos para conhecer nossas instalações e experimentar nossos treinos. 
+          Estamos ansiosas para receber você!
+        </p>
+      </div>
     </section>
   );
 }
 
-export default Informações;
+export default Informacoes;
--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -1,5 +1,11 @@
-import React from 'react';
+import React from 'react'; 
 import styles from './Header.module.css';
+
+// Adicione a fonte Russo One
+const link = document.createElement('link');
+link.href = 'https://fonts.googleapis.com/css2?family=Russo+One&display=swap';
+link.rel = 'stylesheet';
+document.head.appendChild(link);
 
 function Header() {
   return (
@@ -11,8 +17,13 @@ function Header() {
       />
       <nav className={styles.nav}>
         <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Informações</a></li>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <span className={styles.separator}>|</span>
+          <li>
+            <a href="/informacoes">Informações</a>
+          </li>
         </ul>
       </nav>
     </header>
