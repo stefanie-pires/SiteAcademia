d4ecccccaa9d2a0c1a0a461610d557eff9b648a0
Camizella <camilla_padilha@yahoo.com.br>
2024-10-09 21:54:43 -0300

    rotas/login

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Criação da raiz do React
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Renderização do aplicativo
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Para medir o desempenho do aplicativo
 reportWebVitals();
