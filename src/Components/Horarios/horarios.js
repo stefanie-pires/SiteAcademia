18f4abf1e52929d809202ab82403578a6ec4bed3
stefanie-pires <stefanie.gs.sg@gmail.com>
2024-10-29 13:36:52 -0300

    front quase pronto

--- src/Components/Horarios/horarios.js
+++ src/Components/Horarios/horarios.js
@@ -7,30 +7,37 @@ function Horarios() {
       <div className={styles.horariosContainer}>
         <div className={styles.horariosElement}>
           <h2 className={styles.horariosTitle}>Segunda-Feira</h2>
+          <div className={styles.dayInfo}>Aula de dança fit das 9 às 11</div>
           <p className={styles.horariosTime}>06:00 - 22:00</p>
         </div>
         <div className={styles.horariosElement}>
           <h2 className={styles.horariosTitle}>Terça-Feira</h2>
+          <div className={styles.dayInfo}>Treino funcional das 10 às 12</div>
           <p className={styles.horariosTime}>06:00 - 22:00</p>
         </div>
         <div className={styles.horariosElement}>
           <h2 className={styles.horariosTitle}>Quarta-Feira</h2>
+          <div className={styles.dayInfo}>Pilates das 8 às 10</div>
           <p className={styles.horariosTime}>06:00 - 22:00</p>
         </div>
         <div className={styles.horariosElement}>
           <h2 className={styles.horariosTitle}>Quinta-Feira</h2>
+          <div className={styles.dayInfo}>Zumba das 18 às 20</div>
           <p className={styles.horariosTime}>06:00 - 22:00</p>
         </div>
         <div className={styles.horariosElement}>
           <h2 className={styles.horariosTitle}>Sexta-Feira</h2>
+          <div className={styles.dayInfo}>Aula de yoga das 7 às 9</div>
           <p className={styles.horariosTime}>06:00 - 22:00</p>
         </div>
         <div className={`${styles.horariosElement} ${styles.highlight}`}>
           <h2 className={styles.horariosTitle}>Sábado</h2>
+          <div className={styles.dayInfo}>Musculação</div>
           <p className={styles.horariosTime}>09:00 - 18:00</p>
         </div>
         <div className={styles.horariosElement}>
           <h2 className={styles.horariosTitle}>Domingo</h2>
+          <div className={styles.dayInfo}>Fechado</div>
           <p className={styles.horariosTime}>Fechado</p>
         </div>
       </div>
--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -1,35 +1,45 @@
-import React from 'react'; 
+import React, { useState } from 'react'; 
 import styles from './Header.module.css'; // Certifique-se de que o caminho esteja correto
 import { Link } from 'react-router-dom'; // Importando Link para navegação
 
 const Header = () => {
+  const [menuActive, setMenuActive] = useState(false); // State para controlar o menu
+
+  const toggleMenu = () => {
+    setMenuActive(!menuActive); // Alterna o estado do menu
+  };
+
   return (
     <header className={styles.header}>
-      <Link to="/"> {/* Adicionando o Link aqui */}
+      <Link to="/"> 
         <img 
           src="/Logo Fitness Preto e Amarelo Simples.png" 
           alt="Logo da Academia" 
           className={styles.logo} 
         />
       </Link>
-      <nav className={styles.nav}>
+      <nav className={`${styles.nav} ${menuActive ? styles.active : ''}`}>
         <ul>
           <li>
             <Link to="/">Início</Link> 
           </li>
-          <span className={styles.separator}>|</span>
           <li>
             <Link to="/horarios">Horários</Link>
           </li>
-          <span className={styles.separator}>|</span>
           <li>
-            <Link to="/planos">Planos</Link> {/* Link para a página de Planos */}
+            <Link to="/planos">Planos</Link>
           </li>
         </ul>
-        <Link to="/login" className={styles.matriculaButton}>Matrícula Online</Link> {/* Use o Link aqui */}
       </nav>
+      <Link to="/login" className={styles.matriculaButton}>Matrícula Online</Link> 
+      <div className={styles.menuToggle} onClick={toggleMenu}>
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+      </div>
     </header>
   );
 }
 
 export default Header;
+
